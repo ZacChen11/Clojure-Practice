122d25edd77e10703376533a76bfaa679f4dcc04
ZacChen11 <zacchenshuai@gmail.com>
2018-05-29 22:02:56 -0400

    read data from txt files

---
+++ bin/clo_assignment/core_test.clj
@@ -0,0 +1,7 @@
+(ns clo-assignment.core-test
+  (:require [clojure.test :refer :all]
+            [clo-assignment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clo_assignment
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
